dd90450876fdd2ba628629a0c6f5fa8c4987a228
tqhuy04 <tranquanghuy0413@gmail.com>
2026-04-18 22:24:41 +0700

    db

--- Backend/database.py
+++ Backend/database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:130404@localhost/restaurant_booking")
-DATABASE_URL = "mysql+pymysql://root:aBFKwjPHqMYieiGNIXEikFAVrqFdgQaf@roundhouse.proxy.rlwy.net:39960/railway"
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:aBFKwjPHqMYieiGNIXEikFAVrqFdgQaf@roundhouse.proxy.rlwy.net:39960/restaurant_booking")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
